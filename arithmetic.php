d8deac47e59245afaee4e698a9661a0d1036cc29
Matthew Reat <matthew.reat@gmail.com>
2015-07-15 13:27:38 -0500

    adding conditional to apply error message when attempting to divide by zero

--- arithmetic.php
+++ arithmetic.php
@@ -5,56 +5,60 @@
 
 function add($a, $b)
 {
-	if (is_numeric($a) && is_numeric($b)) {
-	    echo $a + $b . PHP_EOL;
+	if (!is_numeric($a) && !is_numeric($b)) {
+		echo "Values are not numeric.  Please re-enter valid numbers as values" . PHP_EOL;
     } else {
-	echo "Values are not numeric.  Please re-enter valid numbers as values" . PHP_EOL;
+	    echo $a + $b . PHP_EOL;
 }
 
 }
 
 function subtract($a, $b)
 {
-	if (is_numeric($a) && is_numeric($b)) {
-	    echo $a - $b . PHP_EOL;
-	} else {
+	if (!is_numeric($a) && !is_numeric($b)) {
 		echo "Values are not numeric.  Please re-enter valid numbers as values" . PHP_EOL;
+    } else {
+	    echo $a - $b . PHP_EOL;
 }
    
 }
 
 function multiply($a, $b)
 {
-	if (is_numeric($a) && is_numeric($b)) {
-	   echo $a * $b . PHP_EOL;
-	} else {
+	if (!is_numeric($a) && !is_numeric($b)) {
 		echo "Values are not numeric.  Please re-enter valid numbers as values" . PHP_EOL;
+    } else {
+	    echo $a * $b . PHP_EOL;
 }
     
 }
 
 function divide($a, $b)
 {
-	if (is_numeric($a) && is_numeric($b)) {
-	    echo $a / $b . PHP_EOL;
-    } else {
+	if (!is_numeric($a) && !is_numeric($b)) {
 		echo "Values are not numeric.  Please re-enter valid numbers as values" . PHP_EOL;
+	} elseif ($b == 0) {
+		echo "Invalid Input.  Cannot divide by 0." . PHP_EOL;
+    } else {
+	    echo $a / $b . PHP_EOL;
 }
    
 }
 
 function modulus($a, $b)
 {
-	if (is_numeric($a) && is_numeric($b)) {
-	    echo $a % $b . PHP_EOL;
-   } else {
+	if (!is_numeric($a) && !is_numeric($b)) {
 		echo "Values are not numeric.  Please re-enter valid numbers as values" . PHP_EOL;
+    } elseif ($b == 0) {
+		echo "Invalid Input.  Cannot divide by 0." . PHP_EOL;
+    } else {
+	    echo $a % $b . PHP_EOL;
 }
  
 }
 
-$a = "stuff";
-$b = 2;
+$a = 10;
+$b = 0;
 
 add($a, $b);
 subtract($a, $b);
