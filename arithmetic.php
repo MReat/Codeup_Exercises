5d05df04e8c2fda4783bed07cbe777019b7b9b5a
Matthew Reat <matthew.reat@gmail.com>
2015-07-15 12:59:34 -0500

    adding new conditional to arithmetic.php that includes is_numeric check

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,64 @@
 <?php
+// $a = 10;
+// $b = 5;
+
 
 function add($a, $b)
 {
-    echo $a + $b;
+	if (is_numeric($a) && is_numeric($b)) {
+	    echo $a + $b . PHP_EOL;
+    } else {
+	echo "Values are not numeric.  Please re-enter valid numbers as values" . PHP_EOL;
+}
+
 }
 
 function subtract($a, $b)
 {
-    // Add code here
+	if (is_numeric($a) && is_numeric($b)) {
+	    echo $a - $b . PHP_EOL;
+	} else {
+		echo "Values are not numeric.  Please re-enter valid numbers as values" . PHP_EOL;
+}
+   
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+	if (is_numeric($a) && is_numeric($b)) {
+	   echo $a * $b . PHP_EOL;
+	} else {
+		echo "Values are not numeric.  Please re-enter valid numbers as values" . PHP_EOL;
+}
+    
 }
 
 function divide($a, $b)
 {
-    // Add code here
+	if (is_numeric($a) && is_numeric($b)) {
+	    echo $a / $b . PHP_EOL;
+    } else {
+		echo "Values are not numeric.  Please re-enter valid numbers as values" . PHP_EOL;
+}
+   
 }
 
-// Add code to test your functions here
+function modulus($a, $b)
+{
+	if (is_numeric($a) && is_numeric($b)) {
+	    echo $a % $b . PHP_EOL;
+   } else {
+		echo "Values are not numeric.  Please re-enter valid numbers as values" . PHP_EOL;
+}
+ 
+}
+
+$a = "stuff";
+$b = 2;
+
+add($a, $b);
+subtract($a, $b);
+multiply($a, $b);
+divide($a, $b);
+modulus($a, $b);
+
