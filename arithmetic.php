671042599397fd9d7da30de1b697b39fc65737c5
Matthew Reat <matthew.reat@gmail.com>
2015-07-15 14:34:40 -0500

    adding error checking function to arithmetic php

--- arithmetic.php
+++ arithmetic.php
@@ -1,16 +1,9 @@
 <?php
-$a = 10;
-$b = 5;
+$a = 20;
+$b = 4;
 
-// function compare($a, $b, $strict) {
-//     if ($strict === true) {
-//         return $a === $b ? 'True' : 'False';
-//     } else {
-//         return $a == $b ? 'True' : 'False';
-//     }
-// }
-
-function errorCheck ($a, $b) {
+function errorCheck ($a, $b) 
+{
 	if (!is_numeric($a)) {
 		echo "{$a} is not numeric.  Please re-enter valid number" . PHP_EOL;
 		return false;
@@ -19,88 +12,45 @@ function errorCheck ($a, $b) {
 		return false;
     } elseif ($b == 0) {
 		echo "Invalid Input = {$b}.  Cannot divide by 0." . PHP_EOL;
+		return false;
+	} else {
+		echo add($a, $b);
+		echo subtract($a, $b);
+		echo multiply($a, $b);
+		echo divide($a, $b);
+		echo modulus($a, $b);
+	}
 }
 
 function add($a, $b)
 {
-	if (!is_numeric($a)) {
-		echo "{$a} is not numeric.  Please re-enter valid number" . PHP_EOL;
-		return false;
-	} elseif (!is_numeric($b)) {
-		echo "{$b} is not numeric.  Please re-enter valid number" . PHP_EOL;
-		return false;
-    } else {
-	    return $a + $b . PHP_EOL;
-}
-
+    return $a + $b . PHP_EOL;
 }
 
 function subtract($a, $b)
 {
-	if (!is_numeric($a)) {
-		echo "{$a} is not numeric.  Please re-enter valid number" . PHP_EOL;
-		return false;
-	} elseif (!is_numeric($b)) {
-		echo "{$b} is not numeric.  Please re-enter valid number" . PHP_EOL;
-		return false;
-    } else {
-	    return $a - $b . PHP_EOL;
-}
-   
+    return $a - $b . PHP_EOL;
 }
 
 function multiply($a, $b)
 {
-	if (!is_numeric($a)) {
-		echo "{$a} is not numeric.  Please re-enter valid number" . PHP_EOL;
-		return false;
-	} elseif (!is_numeric($b)) {
-		echo "{$b} is not numeric.  Please re-enter valid number" . PHP_EOL;
-		return false;
-    } else {
-	    return $a * $b . PHP_EOL;
+    return $a * $b . PHP_EOL;
 }
     
-}
-
 function divide($a, $b)
 {
-	if (!is_numeric($a)) {
-		echo "{$a} is not numeric.  Please re-enter valid number" . PHP_EOL;
-		return false;
-	} elseif (!is_numeric($b)) {
-		echo "{$b} is not numeric.  Please re-enter valid number" . PHP_EOL;
-		return false;
-	} elseif ($b == 0) {
-		echo "Invalid Input = {$b}.  Cannot divide by 0." . PHP_EOL;
-    } else {
-	    return $a / $b . PHP_EOL;
+    return $a / $b . PHP_EOL;
 }
    
-}
-
 function modulus($a, $b)
 {
-	if (!is_numeric($a)) {
-		echo "{$a} is not numeric.  Please re-enter valid number" . PHP_EOL;
-		return false;
-	} elseif (!is_numeric($b)) {
-		echo "{$b} is not numeric.  Please re-enter valid number" . PHP_EOL;
-		return false;
-    } elseif ($b == 0) {
-		echo "Invalid Input = {$b}.  Cannot divide by 0." . PHP_EOL;
-    } else {
-	    return $a % $b . PHP_EOL;
+    return $a % $b . PHP_EOL;
 }
  
-}
+errorCheck($a, $b);
 
-// $a = 10;
-// $b = 0;
 
-echo add($a, $b);
-echo subtract($a, $b);
-echo multiply($a, $b);
-echo divide($a, $b);
-echo modulus($a, $b);
+
+
+
 
