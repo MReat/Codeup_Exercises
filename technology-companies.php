727ae1f088235815fc5bce4def814c52feb62626
Matthew Reat <matthew.reat@gmail.com>
2015-07-16 11:25:57 -0500

    adding technology-company exercise (practicing sorting)

---
+++ technology-companies.php
@@ -0,0 +1,58 @@
+<?php
+
+$companies = [
+    'Sun Microsystems' => [
+        'Vinod Khosla',
+        'Bill Joy',
+        'Andy Bechtolsheim',
+        'Scott McNealy'
+    ],
+    'Silicon Graphics' => [
+        'Jim Clark',
+        'Ed McCracken'
+    ],
+    'Cray' => [
+        'William Norris',
+        'Seymour Cray'
+    ],
+    'NeXT' => [
+        'Steve Jobs',
+        'Avie Tevanian',
+        'Joanna Hoffman',
+        'Bud Tribble',
+        'Susan Kare'
+    ],
+    'Acorn Computers' => [
+        'Steve Furber',
+        'Sophie Wilson',
+        'Hermann Hauser',
+        'Jim Mitchell'
+    ],
+    'MIPS Technologies' => [
+        'Skip Stritter',
+        'John L. Hennessy'
+    ],
+    'Commodore' => [
+        'Yash Terakura',
+        'Bob Russell',
+        'Bob Yannes',
+        'David A. Ziembicki',
+        'Jay Miner'
+    ],
+    'Be Inc' => [
+        'Steve Sakoman',
+        'Jean-Louis Gassée'
+    ]
+];
+
+foreach($companies as $company => $name) {
+    asort($name);    
+    // use below for sorting the employees and then displaying them (for alphabetical)
+    // print_r($company) . PHP_EOL;
+    // print_r($name) . PHP_EOL;
+    // echo "======================" . PHP_EOL;
+       
+}
+
+arsort($companies);
+print_r($companies);
